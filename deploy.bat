23c918a8e8c102d4600d69ea49951c8ffacfb579
anasamir1998-del <ana.samir.1998@gmail.com>
2026-05-16 02:02:44 +0300

    Migrate from Firebase to PHP+MySQL for Hostinger

--- deploy.bat
+++ deploy.bat
@@ -7,17 +7,20 @@ echo.
 
 cd /d "%~dp0"
 
-echo [1/3] Adding all files...
+echo [1/4] Pulling remote changes...
+git pull origin main --no-edit
+echo.
+
+echo [2/4] Adding all files...
 git add -A
 echo.
 
-echo [2/3] Committing changes...
+echo [3/4] Committing changes...
 git commit -m "Migrate from Firebase to PHP+MySQL for Hostinger"
 echo.
 
-echo [3/3] Pushing to remote...
+echo [4/4] Pushing to remote...
 git push origin main
-if errorlevel 1 git push origin master
 echo.
 
 echo ========================================
